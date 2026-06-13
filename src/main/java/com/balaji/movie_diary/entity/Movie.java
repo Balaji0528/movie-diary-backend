d699d003fb8fb931afbaa2dd2824801f00b14e63
Balaji0528 <166303265+Balaji0528@users.noreply.github.com>
2026-06-13 15:33:18 +0530

    Update Movie.java

--- src/main/java/com/balaji/movie_diary/entity/Movie.java
+++ src/main/java/com/balaji/movie_diary/entity/Movie.java
@@ -13,7 +13,7 @@ import lombok.Data;
 @Entity
 public class Movie {
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	@NotNull
