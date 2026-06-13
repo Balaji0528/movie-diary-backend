bfb126d14b3b7aefa20c9193e664ebd0bf2b5c61
Balaji0528 <166303265+Balaji0528@users.noreply.github.com>
2026-06-13 15:32:55 +0530

    Update Diary.java

--- src/main/java/com/balaji/movie_diary/entity/Diary.java
+++ src/main/java/com/balaji/movie_diary/entity/Diary.java
@@ -14,7 +14,7 @@ import lombok.Data;
 public class Diary {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String review;
 	private Float rating;
